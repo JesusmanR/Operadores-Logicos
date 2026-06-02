a33bbb50a45e9f672db501758f92b7e58a4ce7c9
Karoll Orduz <karollorduz1028@gmail.com>
2026-06-01 22:04:24 -0500

    feat: código js, acceso a un sistema #6

---
+++ Punto_D/app.js
@@ -0,0 +1,9 @@
+// Ejercicio D1 - Acceso a un sistema
+let usuario = "admin";
+let contrasena = "1234";
+
+if (usuario === "admin" && contrasena === "1234") {
+  console.log("Acceso permitido");
+} else {
+  console.log("Acceso denegado");
+}
